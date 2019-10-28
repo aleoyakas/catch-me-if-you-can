fffa36baf3382ecabba16c488fcbd6a6862bdfbe
Aleo <34274606+aleoyakas@users.noreply.github.com>
2019-10-28 09:11:32 +0000

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,89 @@
-let gameStarted, ballPosition, ballSize, ballSpeed, timeLimit, seconds, lives, level;
+/* Variables which will be defined set and changed later */
+let gameStarted, ballPosition, ballSize, ballSpeed, timeLimit, totalTime, seconds, lives, level;
+
+/* Constants that will be set on screen load */
+const h = window.innerHeight;
+const w = window.innerWidth;
+
+/* HTML attributes assigned to variables */
+let htmlInstructions = document.getElementById("instructions");
+let htmlStopwatch = document.getElementById("stopwatch");
+let htmlTotalTime = document.getElementById("total-time");
+let htmlBallBox = document.getElementById('ball-box');
+let htmlBallSpeed = document.getElementById("ball-speed");
+let htmlBallSize = document.getElementById("ball-size");
+let htmlLives = document.getElementById("lives");
+let htmlLevel = document.getElementById("level");
+
+const timer = () => t = setTimeout(decreaseTime, 1000);
+
+const decreaseTime = () => {
+  timer();
+
+  seconds--;
+  stopwatch.innerHTML = seconds;
+
+  totalTime++;
+  htmlTotalTime.innerHTML = totalTime;
+
+  if (seconds == 0) {
+    clearTimeout(t);
+    lives--;
+    if (lives === 0) {
+      alert(`You didn't catch the ball in time and you're out of lives! You got to level ${level} in ${totalTime} seconds!`);
+      restartGame();
+    }
+    else if (lives === 1) alert("You didn't catch the ball in time! You're on your last life!");
+    else alert(`You didn't catch the ball in time! You have ${lives} lives left!`);
+    
+    levelSetup();
+  }
+}
+
+const moveBall = () => {
+  $("#ball-box").animate({
+    top: h*Math.random() + "px",
+    left: w*Math.random() + "px"
+  }, ballSpeed, () => {
+    if (gameStarted) {
+      moveBall();
+    } else {
+      levelSetup();
+    }
+  });
+};
+
+const clickBall = () => {
+  gameStarted = !gameStarted;
+  if (!gameStarted) {
+    clearTimeout(t);
+    alert(`You caught the ball with ${seconds}s left!`);
+    level++
+    if (level % 2 === 1) {
+      ballPosition++;
+      ballSize--;
+    } else ballSpeed += 5;
+  } else {
+    decreaseTime();
+    moveBall();
+    htmlInstructions.innerHTML = 'Click the ball as quickly as you can!';
+  }
+}
+
+const levelSetup = () => {
+  gameStarted = false;
+  seconds = timeLimit;
+  
+  htmlInstructions.innerHTML = 'Click the ball... if you dare...';
+  htmlStopwatch.innerHTML = timeLimit;
+  htmlTotalTime.innerHTML = totalTime;
+  htmlLives.innerHTML = lives;
+  htmlLevel.innerHTML = level;
+  htmlBallSize.innerHTML = ballSize;
+  htmlBallSpeed.innerHTML = ballSpeed;
+  
+  htmlBallBox.setAttribute("style",`left: ${ballPosition}vw; top: ${ballPosition}vh; width: ${ballSize}vw; height: ${ballSize};`)
+};
 
 const restartGame = () => {
   if (window.innerWidth <= 700) {
@@ -10,82 +95,11 @@ const restartGame = () => {
   }
   
   ballSpeed = 1000;
+  totalTime = 0;
   timeLimit = 30;
   lives = 5;
   level = 1;
-  gameSetup();
-}
-
-const timer = () => t = setTimeout(increaseTime, 1000);
-
-const increaseTime = () => {
-  timer();  
-  seconds--;  
-  stopwatch.textContent = seconds;
-  if (stopwatch.textContent == 00) {
-    clearTimeout(t);
-    lives--;
-    if (lives === 0) {
-      alert(`You didn't catch the ball in time and you're out of lives! You got to level ${level}`);
-      restartGame();
-    }
-    else if (lives === 1) alert("You didn't catch the ball in time! You're on your last life!");
-    else alert(`You didn't catch the ball in time! You have ${lives} lives left!`);
-    
-    gameSetup();
-  }
-}
-
-const gameSetup = () => {
-  gameStarted = false;
-  
-  document.getElementById("instructions").innerHTML = 'Click the ball... if you dare...';
-  document.getElementById("stopwatch").innerHTML = timeLimit;
-  seconds = timeLimit;
-  document.getElementById("lives").innerHTML = lives;
-  document.getElementById("level").innerHTML = level;
-  document.getElementById("ball-size").innerHTML = ballSize;
-  document.getElementById("ball-speed").innerHTML = ballSpeed;
-  
-  document.getElementById('ball-box').style.left = `${ballPosition}vw`;
-  document.getElementById('ball-box').style.top = `${ballPosition}vh`;
-  document.getElementById('ball-box').style.width = `${ballSize}vw`;
-  document.getElementById('ball-box').style.height = `${ballSize}vh`;
-};
-
-const randomRgbValue = () => Math.floor(Math.random()*256);
-
-const moveBall = () => {
-  const h = window.innerHeight;
-  const w = window.innerWidth;
-  $("#ball-box").animate({
-    top: h*Math.random() + "px",
-    left: w*Math.random() + "px"
-  }, ballSpeed, () => {
-    if (gameStarted) {
-      moveBall();
-    } else {
-      gameSetup();
-    }
-  });
-};
-
-const clickBall = () => {
-  gameStarted = !gameStarted;
-  if (!gameStarted) {
-    clearTimeout(t);
-    let timeTaken = document.getElementById("stopwatch").innerHTML;
-    alert(`You caught the ball with ${timeTaken}s left!`);
-    level++
-    if (level % 2 === 1) {
-      ballPosition++;
-      ballSize--;
-    } else ballSpeed = ballSpeed + 5;
-  } else {
-    increaseTime();
-    moveBall();
-    $("#instructions").html('Click the ball as quickly as you can!');
-  }
+  levelSetup();
 }
 
 restartGame();
